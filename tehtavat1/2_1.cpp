d3562e087182f89aac54aea6df64ca186bce6967
Harles-A <harles.asu@gmail.com>
2025-10-26 23:10:07 +0200

    task 2_1

---
+++ tehtavat1/2_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string nimi;
+    cout << "Anna nimesi: " ;
+    cin >> nimi;
+    cout << "Terve, " << nimi << "!" << endl;
+    return 0;
+}
