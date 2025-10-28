0e98ef7f8c8797eb43595559b2dc0ffd74ef4b3f
Harles-A <harles.asu@gmail.com>
2025-10-28 14:07:52 +0200

    tuntitehtavat v2

--- tehtavat2/tehtava5_02.cpp
+++ tehtavat2/tehtava5_02.cpp
@@ -2,3 +2,19 @@
 // toinen osoitin, joka osoittaa äskeiseen osoittimeen. Muuta sen jälkeen
 // em. kokonaisluvun arvoa jälkimmäisen osoittimen avulla. Vihje: **
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x = 50;
+
+    int* y = &x; // y osottaa x:ään
+    int** z = &y; // z osoittaa y:hyn
+
+    **z = 200;
+
+    cout << x << endl;
+
+}
--- tehtavat2/tehtava6_02.cpp
+++ tehtavat2/tehtava6_02.cpp
@@ -1,3 +1,13 @@
 // Määrittele osoitin, joka alkaa osoittaa nullptr-arvoon. Tarkista ohjelmassa, onko
 // osoittimen arvo nullptr ennen kuin käytät sitä, ja tulosta viesti tilanteesta.
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int* p = nullptr; // Same as 0 or NULL
+
+    return 0;
+}
